ba6dd7c8443063b7ce8b84f51837ced860414369
Mark Chmarny <mchmarny@users.noreply.github.com>
2023-03-20 18:07:14 -0700

    With params support in GitHub action (#8)
    * refactor gha to use with params
    
    * update output param

---
+++ cmd/action/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"context"
+
+	"github.com/GoogleCloudPlatform/aactl/pkg/types"
+	"github.com/GoogleCloudPlatform/aactl/pkg/vul"
+	gha "github.com/sethvargo/go-githubactions"
+)
+
+var (
+	version = "v0.0.1-default"
+	commit  = "none"
+	date    = "unknown"
+)
+
+func main() {
+	a := gha.WithFieldsMap(map[string]string{
+		"version": version,
+		"commit":  commit,
+		"date":    date,
+	})
+
+	a.Infof("starting action")
+	defer a.Infof("action completed")
+
+	project := a.GetInput("project")
+	source := a.GetInput("source")
+	file := a.GetInput("file")
+	format := a.GetInput("format")
+
+	if project == "" || source == "" || file == "" || format == "" {
+		a.Fatalf("required parameter not provided, got project=%s, source=%s, file=%s, format=%s", project, source, file, format)
+	}
+
+	f, err := types.ParseSourceFormat(format)
+	if err != nil {
+		a.Fatalf("error parsing source format, got %s", err)
+	}
+
+	opt := &types.ImportOptions{
+		Project: project,
+		Source:  source,
+		File:    file,
+		Format:  f,
+	}
+
+	if err := vul.Import(context.Background(), opt); err != nil {
+		a.Fatalf("error importing vulnerabilities, got %s", err)
+	}
+
+	// TODO: add more meaningful output params (e.g. number of vulns imported)
+	a.SetOutput("result", "ok")
+}
--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/Jeffail/gabs/v2 v2.7.0
 	github.com/pkg/errors v0.9.1
 	github.com/rs/zerolog v1.29.0
+	github.com/sethvargo/go-githubactions v1.1.0
 	github.com/stretchr/testify v1.8.2
 	github.com/urfave/cli/v2 v2.25.0
 	golang.org/x/net v0.8.0
@@ -32,6 +33,7 @@ require (
 	github.com/mattn/go-isatty v0.0.17 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/sethvargo/go-envconfig v0.8.0 // indirect
 	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
 	go.opencensus.io v0.24.0 // indirect
 	golang.org/x/oauth2 v0.6.0 // indirect
